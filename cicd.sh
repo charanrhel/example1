b193f09670b8a409d6d17b7cf240749b17b49b58
charanrhel <charan.rhel@gmail.com>
2023-01-30 16:18:18 -0800

    added maven to file

--- cicd.sh
+++ cicd.sh
@@ -10,3 +10,8 @@ amazon-linux-extras install java-openjdk11 -y
 yum install jenkins -y
 systemctl enable jenkins
 systemctl start jenkins
+
+# wget https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.tar.gz
+# tar xzvf apache-maven-3.8.7-bin.tar.gz 
+# mv apache-maven-3.8.7 maven3
+# mv maven3/ /opt/
